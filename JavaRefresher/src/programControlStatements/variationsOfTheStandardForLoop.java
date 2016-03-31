115607e8bd2c3557aa64b7a9e7479a46aee6da1c
Warren <darwinsmongoose@gmail.com>
2016-03-31 12:03:07 -0500

    Looking at multiple control variables and independent conditional expressions.

--- JavaRefresher/src/programControlStatements/variationsOfTheStandardForLoop.java
+++ JavaRefresher/src/programControlStatements/variationsOfTheStandardForLoop.java
@@ -1,10 +1,27 @@
 package programControlStatements;
 
+import java.io.IOException;
+
 public class variationsOfTheStandardForLoop {
-
-	public static void main(String[] args) {
+	/*Having a look at some of the more quirky variations of the standard for loop.
+	 * 
+	 * 
+	 * 
+	 */
+	public static void main(String[] args) throws IOException {
 		
-
+		int i,j;
+		//using two seperate control variables. Will stop at 5/5
+		for(i = 0, j = 10; i < j; i++, j--) {
+			System.out.println("i and j: " + i + " " + j);
+		}
+		
+		//using a seperate control condition outside of the control variables. Can be any
+		//valid boolean expression.
+		System.out.println("Press 's' to stop");
+		for(i = 0; (char) System.in.read() != 's'; i++){
+			System.out.println("Pass #"+ i);
+		}
 	}
 
 }
