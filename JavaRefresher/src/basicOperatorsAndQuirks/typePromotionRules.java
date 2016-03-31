f020079db1aab3c11efcf746000f2f11d964e533
Warren <darwinsmongoose@gmail.com>
2016-03-30 22:27:27 -0500

    char to int type promotion along side using double casting on ints.

--- JavaRefresher/src/basicOperatorsAndQuirks/typePromotionRules.java
+++ JavaRefresher/src/basicOperatorsAndQuirks/typePromotionRules.java
@@ -19,7 +19,23 @@ public class typePromotionRules {
 		b = (byte) (b * b);
 		System.out.println("i and b: " + i + " " + b);
 		
-
+		char ch1 = 'A', ch2 = '!';
+		i = (ch1 + ch2);
+		ch1 = (char) (ch1 + ch2);
+		
+		System.out.print("A + ! as an integer value is: " + i +"\n");
+		System.out.println("A + !: " + ch1 + "\n");
+		
+		//A cast to a double can be used to get fractional components from
+		//a integer division. Enabling a better precision from these operations.
+		
+		for(int j = 0; j < 5; j++){
+			
+			System.out.print(j + "/3: "+j/3 + "\n");
+			System.out.print(j + "/3 with fractions :"+ (double)j/3);
+			System.out.println();
+			
+		}
 	}
 
 }
