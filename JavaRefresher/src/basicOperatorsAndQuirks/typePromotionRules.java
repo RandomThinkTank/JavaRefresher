2b28d7754d910221f89c361e976869d27b03e2e8
Warren <darwinsmongoose@gmail.com>
2016-03-30 22:16:01 -0500

    promotion of bytes to ints during operations lead to unexpected results.

--- JavaRefresher/src/basicOperatorsAndQuirks/typePromotionRules.java
+++ JavaRefresher/src/basicOperatorsAndQuirks/typePromotionRules.java
@@ -1,8 +1,23 @@
 package basicOperatorsAndQuirks;
 
 public class typePromotionRules {
-
+	/*Checking out the type promotion rules java uses when multiple types are
+	 * used in the same expression. Java will promote char, byte and short values to ints
+	 * -> longs ->floats -> double.
+	 * 
+	 * This type promotion can lead to unexpected results. For example, the outcome of
+	 * two byte values will be an int as shown below.
+	 * 
+	 */
 	public static void main(String[] args) {
+		byte b;
+		int i;
+		
+		b = 10;
+		i = b * b; //No cast is needed in this case.
+		
+		b = (byte) (b * b);
+		System.out.println("i and b: " + i + " " + b);
 		
 
 	}
