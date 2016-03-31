9046b112abdfa8b575e9efd2c2cabbe9bf8d09b9
Warren <darwinsmongoose@gmail.com>
2016-03-30 21:29:58 -0500

    Initial Commit

---
+++ JavaRefresher/src/BasicOperatorsAndQuirks/LogicalOperators.java
@@ -0,0 +1,11 @@
+package BasicOperatorsAndQuirks;
+
+public class LogicalOperators {
+	/*A small refresher on logical operators. Really more to test out
+	 * using github as a versioning system instead of SVN.
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
