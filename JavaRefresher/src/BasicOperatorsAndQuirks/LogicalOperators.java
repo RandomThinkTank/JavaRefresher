45234aa721a2a0f8e6413a40c4ee458c62bdeb3b
Warren <darwinsmongoose@gmail.com>
2016-03-30 21:40:13 -0500

    Table of logical operator outputs. Set the table formatting.

--- JavaRefresher/src/BasicOperatorsAndQuirks/LogicalOperators.java
+++ JavaRefresher/src/BasicOperatorsAndQuirks/LogicalOperators.java
@@ -5,6 +5,17 @@ public class LogicalOperators {
 	 * using github as a versioning system instead of SVN.
 	 */
 	public static void main(String[] args) {
+		boolean x, y;
+		
+		System.out.println("X\tY\tAND\tOR\tXOR\tNOT");
+		
+		x = true;
+		y = true;
+		System.out.print(x + "\t" + y + "\t");
+		System.out.print((x&y) + "\t" + (x|y) + "\t");
+		System.out.println((x ^ y) + "\t" + (!x));
+		
+		y = false;
 		
 	}
 
