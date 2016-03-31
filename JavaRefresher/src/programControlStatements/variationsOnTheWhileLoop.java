db885d44fb95a01fe99b898291857976ecf3c9f6
Warren <darwinsmongoose@gmail.com>
2016-03-31 12:17:07 -0500

    initial commit. Just looking back at while loop syntax.. nothing new tbh.

---
+++ JavaRefresher/src/programControlStatements/variationsOnTheWhileLoop.java
@@ -0,0 +1,29 @@
+package programControlStatements;
+
+public class variationsOnTheWhileLoop {
+	/*Having a look at some of the variations of the while loop syntax.
+	 * 
+	 */
+	public static void main(String[] args) {
+		char ch;
+		ch = 'a';
+		while(ch <= 'z') {
+			System.out.println(ch);
+			ch ++;
+		}
+		
+		//Compute powers of 2.
+		int e, result;
+		for(int i = 0; i < 10; i++){
+			result = 1;
+			e = i;
+			while(e > 0) {
+				result *= 2;
+				e--;
+			}
+			
+			System.out.println("2 to the " + i + "th power is " + result );
+		}
+	}
+
+}
