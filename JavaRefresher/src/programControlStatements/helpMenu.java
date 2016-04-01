b34360c3aacc0350cbfe3adb49e7a0096cb6728f
Warren <darwinsmongoose@gmail.com>
2016-03-31 19:04:20 -0500

    Slightly improved version of the help system using flow statements.

--- JavaRefresher/src/programControlStatements/helpMenu.java
+++ JavaRefresher/src/programControlStatements/helpMenu.java
@@ -3,37 +3,65 @@ package programControlStatements;
 import java.io.IOException;
 
 public class helpMenu {
-	/*Simple Java Syntax help menu using some of the concepts touched upon recently.
-	 * 
-	 * 
+	/*
+	 * An improved version of the help system developed previously.
 	 * 
 	 */
 	public static void main(String[] args) throws IOException {
+		char choice, ignore;
 		
-		System.out.println("Help on:");
-		System.out.println("1. if");
-		System.out.println("2. switch");
-		System.out.println("Choose one:");
+		do {
+			System.out.println("Help on");
+			System.out.println("  1. if");
+			System.out.println("  2.switch");
+			System.out.println("  3.for");
+			System.out.println("  4.while");
+			System.out.println("  5.do-while\n");
+			System.out.println("Choose one: ");
+			
+			choice = (char)System.in.read();
+			
+			do{
+				ignore = (char) System.in.read();
+			}while(ignore != '\n');
+		}while(choice < '1' | choice > '5');
 		
-		char choice = (char) System.in.read();
-		
-		switch(choice) {
-		case '1':
-			System.out.println("The if:\n");
-			System.out.println("if(condition) statement;");
-			System.out.println("else statement");
-			break;
-		case '2':
-			System.out.println("The switch:\n");
-			System.out.println("switch(expression) {");
-			System.out.println("  case constant:");
-			System.out.println("  break;");
-			System.out.println(" // ...");
-			System.out.println("}");
-			break;
-		}
-		
-
+		System.out.println("\n");
+	switch(choice) {
+	case '1':
+		System.out.println("The if:\n");
+		System.out.println("if(condition) statement;");
+		System.out.println("else statement");
+		break;
+	case '2':
+		System.out.println("The switch:\n");
+		System.out.println("switch(expression) {");
+		System.out.println("  case constant:");
+		System.out.println("  break;");
+		System.out.println(" // ...");
+		System.out.println("}");
+		break;
+	case '3':
+		System.out.println("The for loop:\n");
+		System.out.println("for(initialisation; condition; iteration) {");
+		System.out.println("statements");
+		System.out.println("}");
+		break;
+	case '4':
+		System.out.println("The while loop:\n");
+		System.out.println("while(conditional) {");
+		System.out.println("statements");
+		System.out.println("}");
+		break;
+	case '5':
+		System.out.println("The do while loop\n");
+		System.out.println("do {");
+		System.out.println("statements");
+		System.out.println("} while (condition)");
+		break;
+	default:
+		System.out.println("no dice. We'll never hit this :P");
+		break;
 	}
-
+	}
 }
