8fd7332824de010a521888f297425594c6229c38
Warren <darwinsmongoose@gmail.com>
2016-04-02 14:30:49 -0500

    Added infinite loop and entries for continue and break statements.

--- JavaRefresher/src/programControlStatements/helpMenu.java
+++ JavaRefresher/src/programControlStatements/helpMenu.java
@@ -10,58 +10,73 @@ public class helpMenu {
 	public static void main(String[] args) throws IOException {
 		char choice, ignore;
 		
-		do {
-			System.out.println("Help on");
-			System.out.println("  1. if");
-			System.out.println("  2.switch");
-			System.out.println("  3.for");
-			System.out.println("  4.while");
-			System.out.println("  5.do-while\n");
-			System.out.println("Choose one: ");
+		for(;;){
+			do {
+				System.out.println("Help on");
+				System.out.println("  1. if");
+				System.out.println("  2.switch");
+				System.out.println("  3.for");
+				System.out.println("  4.while");
+				System.out.println("  5.do-while");
+				System.out.println("  6.break");
+				System.out.println("  7.continue\n");
+				System.out.println("Choose one (q to quit): ");
+				
+				choice = (char)System.in.read();
+				
+				do{
+					ignore = (char) System.in.read();
+				} while(ignore != '\n');
+				
+			} while(choice < '1' | choice > '7' & choice !='q');
 			
-			choice = (char)System.in.read();
-			
-			do{
-				ignore = (char) System.in.read();
-			}while(ignore != '\n');
-		}while(choice < '1' | choice > '5');
-		
 		System.out.println("\n");
-	switch(choice) {
-	case '1':
-		System.out.println("The if:\n");
-		System.out.println("if(condition) statement;");
-		System.out.println("else statement");
-		break;
-	case '2':
-		System.out.println("The switch:\n");
-		System.out.println("switch(expression) {");
-		System.out.println("  case constant:");
-		System.out.println("  break;");
-		System.out.println(" // ...");
-		System.out.println("}");
-		break;
-	case '3':
-		System.out.println("The for loop:\n");
-		System.out.println("for(initialisation; condition; iteration) {");
-		System.out.println("statements");
-		System.out.println("}");
-		break;
-	case '4':
-		System.out.println("The while loop:\n");
-		System.out.println("while(conditional) {");
-		System.out.println("statements");
-		System.out.println("}");
-		break;
-	case '5':
-		System.out.println("The do while loop\n");
-		System.out.println("do {");
-		System.out.println("statements");
-		System.out.println("} while (condition)");
-		break;
-	default:
-		System.out.println("no dice. We'll never hit this :P");
-		break;
-	}
+		if(choice == 'q') break;	
+		
+		switch(choice) {
+		case '1':
+			System.out.println("The if:\n");
+			System.out.println("if(condition) statement;");
+			System.out.println("else statement");
+			break;
+		case '2':
+			System.out.println("The switch:\n");
+			System.out.println("switch(expression) {");
+			System.out.println("  case constant:");
+			System.out.println("  break;");
+			System.out.println(" // ...");
+			System.out.println("}");
+			break;
+		case '3':
+			System.out.println("The for loop:\n");
+			System.out.println("for(initialisation; condition; iteration) {");
+			System.out.println("statements");
+			System.out.println("}");
+			break;
+		case '4':
+			System.out.println("The while loop:\n");
+			System.out.println("while(conditional) {");
+			System.out.println("statements");
+			System.out.println("}");
+			break;
+		case '5':
+			System.out.println("The do while loop\n");
+			System.out.println("do {");
+			System.out.println("statements");
+			System.out.println("} while (condition)");
+			break;
+		case '6':
+			System.out.println("The break statement\n");
+			System.out.println("break; or break label;");
+			break;
+		case '7':
+			System.out.println("The continue statement\n");
+			System.out.println("continue; or continue label;");
+			break;
+		default:
+			System.out.println("no dice. We'll never hit this :P");
+			break;
+		}
+		}
 	}
 }
