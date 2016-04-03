43a004efc3a759eb897177f9e53cefb6239ce3a0
Warren <darwinsmongoose@gmail.com>
2016-04-03 15:28:53 -0500

    put and get methods of queue used to store and fetch the alphabet.

--- JavaRefresher/src/otherDataTypesAndCollections/Queues/QueueDemo.java
+++ JavaRefresher/src/otherDataTypesAndCollections/Queues/QueueDemo.java
@@ -4,8 +4,20 @@ public class QueueDemo {
 
 	public static void main(String[] args) {
 		
-		Queue alphabetQueue = new Queue(24);
+		Queue alphabetQueue = new Queue(26);
+		char letter = 'a';
+		char ch;
 		
+		for(int i = 0; i < 26; i++) {
+			alphabetQueue.put(letter++);
+		}
+		
+		System.out.println("Contents of the alphabet queue.");
+		
+		for(int i = 0; i < 26; i++) {
+			ch = alphabetQueue.get();
+			System.out.print(ch + " ");
+		}
 
 	}
 
