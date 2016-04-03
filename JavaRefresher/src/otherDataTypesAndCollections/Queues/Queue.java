705aa69a010c3d325c55d61644669c57e43f27a8
Warren <darwinsmongoose@gmail.com>
2016-04-03 15:20:13 -0500

    implemented get and put methods (push and pop)

--- JavaRefresher/src/otherDataTypesAndCollections/Queues/Queue.java
+++ JavaRefresher/src/otherDataTypesAndCollections/Queues/Queue.java
@@ -11,4 +11,23 @@ public class Queue {
 		q = new char[size]; //allow a new 'queue' of any size to be instantiated through a constructor.
 		putloc = getloc = 0; //Initialization of the indices used.
 	}
+	
+	void put(char ch) {
+		if(putloc == q.length) {
+			System.out.println("*** Queue is full ***");
+			return;
+		}
+		
+		q[putloc++] = ch;
+	}
+	
+	char get() {
+		if(getloc == putloc) {
+			System.out.println("*** Queue is empty ***");
+			return (char) 0;
+		}
+		return q[getloc++];
+		
+	}
 }
+
