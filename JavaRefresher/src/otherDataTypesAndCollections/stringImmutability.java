25be62e57248695516048eca91cfd59ec7d7e04a
Warren <darwinsmongoose@gmail.com>
2016-04-06 16:46:21 -0500

    looking at the String, StringBuffer and StringBuilder classes

---
+++ JavaRefresher/src/otherDataTypesAndCollections/stringImmutability.java
@@ -0,0 +1,28 @@
+package otherDataTypesAndCollections;
+
+public class stringImmutability {
+
+	public static void main(String[] args) {
+		String theImmutableString = "theImmutableString";
+		
+		theImmutableString = "catsPyjamas"; //Now pointing to the string object with the
+											//character sequence of "catsPyjamas"
+		
+		StringBuffer aMutableString = new StringBuffer("aMutableString");
+		
+		aMutableString.append(" can change!");
+		
+		System.out.println("Contents of StringBuffer object: " + aMutableString);
+		
+		String aSubstringOfImmutableString = theImmutableString.substring(5,10);
+		
+		//Demonstrating immutability - taking the substring of a string does not alter the original string at all.
+		System.out.println(aSubstringOfImmutableString);
+		System.out.println(theImmutableString);
+		
+		
+		
+		
+	}
+
+}
