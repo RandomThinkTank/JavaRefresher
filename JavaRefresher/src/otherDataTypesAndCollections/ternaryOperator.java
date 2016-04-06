d09373748774aaff344b835fec268f7b8e7befab
Warren <darwinsmongoose@gmail.com>
2016-04-06 17:15:25 -0500

    preventing a divide by zero scenario using ternary operators.

---
+++ JavaRefresher/src/otherDataTypesAndCollections/ternaryOperator.java
@@ -0,0 +1,15 @@
+package otherDataTypesAndCollections;
+
+public class ternaryOperator {
+
+	public static void main(String[] args) {
+		int result;
+		
+		for(int i = -6; i < 6; i++){
+			result = i != 0 ? 100 / i : 0;
+			if(i !=0) System.out.println("100 / " + i +" is " +result);
+		}
+
+	}
+
+}
