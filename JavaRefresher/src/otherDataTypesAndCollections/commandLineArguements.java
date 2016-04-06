efd690facfe2dc61b1fd77699d4fed2d9fa469d9
Warren <darwinsmongoose@gmail.com>
2016-04-06 16:57:28 -0500

    having a look at command line arguements and manipulating the args param

---
+++ JavaRefresher/src/otherDataTypesAndCollections/commandLineArguements.java
@@ -0,0 +1,12 @@
+package otherDataTypesAndCollections;
+
+public class commandLineArguements {
+	/*Takes in arguements passed from the command line to the main method. Looks up a 2D
+	 * array of phone numbers for a paticular name.
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
