c5edc6c4c8f84dc700114ed477e9bdcad1fd21c0
Warren <darwinsmongoose@gmail.com>
2016-04-06 17:49:49 -0500

    looking at the private and public access modifiers, protected to come later.

---
+++ JavaRefresher/src/moreMethodsAndClasses/accessModifiers.java
@@ -0,0 +1,17 @@
+package moreMethodsAndClasses;
+
+public class accessModifiers {
+	private int alpha;
+	public int beta;
+	int gamma;
+	
+	public static void main(String[] args) {
+		
+
+	}
+	
+	void setAlpha(int a){
+		
+	}
+
+}
