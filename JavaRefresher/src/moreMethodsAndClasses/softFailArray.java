092c6620ffd0189a7fa405579dd5be69f1d658d5
Warren <darwinsmongoose@gmail.com>
2016-04-08 14:52:36 -0500

    Imp of getter and out of bounds check.

--- JavaRefresher/src/moreMethodsAndClasses/softFailArray.java
+++ JavaRefresher/src/moreMethodsAndClasses/softFailArray.java
@@ -1,5 +1,24 @@
 package moreMethodsAndClasses;
 
 public class softFailArray {
+	
+	private int a[]; //the private array being encapsulated
+	private int errval; //value to return should the getter fail.
+	public int length;
+	
+	public softFailArray(int size, int errv) {
+		a = new int[size];
+		length = size;
+		errval = errv;
+	}
+	
+	public int get(int index) {
+		if(indexOk(index)) return a[index];
+		return a[index];
+	}
 
+	private boolean indexOk(int index) {
+		if(index >=0 && index < length) return true;
+		return false;
+	}
 }
