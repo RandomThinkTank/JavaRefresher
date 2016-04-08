0a33072798169c202f68b96d36685d57ce116557
Warren <darwinsmongoose@gmail.com>
2016-04-08 15:21:47 -0500

    impl a queue class using access modifiers.

---
+++ JavaRefresher/src/moreMethodsAndClasses/Queue.java
@@ -0,0 +1,12 @@
+package moreMethodsAndClasses;
+
+public class Queue {
+	private char q[];
+	private int putloc, getloc;
+	
+	Queue(int size) {
+		q = new char[size];
+		putloc = getloc = 0;
+	}
+	
+}
