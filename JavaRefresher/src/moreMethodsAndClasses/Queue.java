e089a7c02d06d6bbc73679be31c1e154a25ccd48
Warren <darwinsmongoose@gmail.com>
2016-04-08 15:29:05 -0500

    impl getter and setter for the queue. Strictly enforced FIFO using access modifiers.

--- JavaRefresher/src/moreMethodsAndClasses/Queue.java
+++ JavaRefresher/src/moreMethodsAndClasses/Queue.java
@@ -9,4 +9,21 @@ public class Queue {
 		putloc = getloc = 0;
 	}
 	
+	void put(char ch) {
+		if(putloc == q.length) {
+			System.out.println(" -- Queue is full --");
+			return;
+		}
+		q[putloc++] = ch;
+	}
+	
+	char get() {
+		if(getloc == putloc) {
+			System.out.println("-- Queue is empty --");
+			return (char) 0;
+		}
+		
+		return q[getloc++];
+	}
+	
 }
