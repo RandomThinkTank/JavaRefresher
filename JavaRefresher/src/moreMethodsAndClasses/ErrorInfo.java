1488a735b7152472bb622dedb99e73705c83ed90
Warren <darwinsmongoose@gmail.com>
2016-04-10 20:51:30 -0500

    method getErrorInfo, returns an error type message along with a severity number. Will be returning this message as an object next, with the idea that this message can be mutated as it bubbles up to the user.

--- JavaRefresher/src/moreMethodsAndClasses/ErrorInfo.java
+++ JavaRefresher/src/moreMethodsAndClasses/ErrorInfo.java
@@ -1,5 +1,18 @@
 package moreMethodsAndClasses;
 
 public class ErrorInfo {
-
+	String msgs[] = {
+		"Output Error",
+		"Input Error",
+		"Disk Full",
+		"Index OOB"
+	};
+	
+	int severity[] = {2,2,3,4};
+	String getErrorInfo(int errorCode) {
+		if(errorCode >= 1 && errorCode < msgs.length ) {
+			return msgs[errorCode] + " Severity: " + severity[errorCode];  
+		}
+		return "Invalid Error Code";
+	}
 }
