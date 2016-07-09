a1109fbad335c733a7558a2c6673e2a904245ab9
Warren <darwinsmongoose@gmail.com>
2016-07-08 20:35:09 -0500

    Small class changing the state of the lights using the enum constants of TrafficLightColor

--- JavaRefresher/src/enumerations/TrafficLightSimulator.java
+++ JavaRefresher/src/enumerations/TrafficLightSimulator.java
@@ -22,8 +22,66 @@ public class TrafficLightSimulator implements Runnable {
 
 	@Override
 	public void run() {
+		while(!stop){
+			try{
+				switch(tlc) {
+				case GREEN:
+					Thread.sleep(10000);
+				break;
+				case YELLOW:
+					Thread.sleep(2000);
+				break;
+				case RED:
+					Thread.sleep(12000);
+				break;
+				}
+			}catch(InterruptedException ex){
+				System.out.println("Thread " + thrd.getName() + " had an exception");
+				ex.printStackTrace();
+			}
+			changeColor();
+		}
 		
+	}
+
+	private void changeColor() {
+		switch(tlc) {
+		case RED:
+			tlc = TrafficLightColor.GREEN;
+			break;
+		case YELLOW:
+			tlc = TrafficLightColor.RED;
+			break;
+		case GREEN:
+			tlc = TrafficLightColor.YELLOW;
+			break;
+		}
 		
+		changed = true;
+		notify(); //signal that light has changed.
+	}
+	
+	/*This method won't return until changedColor calls notify.
+	 * Basically when the light ends up changing state.
+	 * 
+	 */
+	synchronized void waitForChange(){
+		try{
+			while(!changed)
+				wait();
+			changed = false;
+		} catch(InterruptedException ex){
+			System.out.println(ex);
+		}
+	}
+	
+	
+	synchronized TrafficLightColor getColor() {
+		return tlc;	
+	}
+	
+	synchronized void cancel(){
+		stop = true;
 	}
 
 }
