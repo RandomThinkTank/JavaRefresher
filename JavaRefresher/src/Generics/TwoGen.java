fc1dc700e9408bc3e63dbc21147b7508b35f8518
Warren <darwinsmongoose@gmail.com>
2016-07-09 16:36:05 -0500

    A generic class with two type parameters demo.

---
+++ JavaRefresher/src/Generics/TwoGen.java
@@ -0,0 +1,18 @@
+package Generics;
+
+class TwoGen<T, V> { //Notice this syntax for using two types.
+	T ob1;
+	V ob2;
+	
+	TwoGen(T o1, V o2){
+		ob1  = o1;
+		ob2 = o2;
+	}
+	
+	//Show types of V and T.
+	void showTypes(){
+		
+	}
+	
+	
+}
