d7780cb70e30c6301006ce82654de379db393357
Warren <darwinsmongoose@gmail.com>
2016-07-11 21:24:47 -0500

    new obj used to demo output from the generic constructor. Nothing interesting.

--- JavaRefresher/src/Generics/GenConDemo.java
+++ JavaRefresher/src/Generics/GenConDemo.java
@@ -3,7 +3,9 @@ package Generics;
 public class GenConDemo {
 
 	public static void main(String[] args) {
-
+		Summation ob = new Summation(4.0);
+		
+		System.out.println("summation of 4.0 is " + ob.getSum());
 
 	}
 
