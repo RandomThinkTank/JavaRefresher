3c80b9c0365cd6e4ebeb9b09dd920f860e0ca147
Warren <darwinsmongoose@gmail.com>
2016-07-11 21:12:07 -0500

    Provided implementation of method checking array contents.

--- JavaRefresher/src/Generics/genericMethodDemo.java
+++ JavaRefresher/src/Generics/genericMethodDemo.java
@@ -8,6 +8,13 @@ public class genericMethodDemo {
 	 * returns true if the elements match.
 	 */
 	static <T extends Comparable<T>, V extends T> boolean arraysEqual(T[] x, V[] y){
+		//Check the lengths first.
+		if(x.length != y.length) return false;
+		
+		//Then check each element.
+		for(int i = 0; i < x.length; i++){
+			if(!x[i].equals(y[i])) return false;
+		}
 		return true;
 		
 	}
